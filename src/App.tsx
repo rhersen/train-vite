8914da9584dd529785241cdb090f0389118346ca
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-09-10 12:11:23 +0200

    remove example code

--- src/App.tsx
+++ src/App.tsx
@@ -26,62 +26,35 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button
-            type="button"
-            onClick={() =>
-              fetch(
-                "/.netlify/functions/node-fetch?trainId=7228&until=2021-09-11T23:59:59"
-              )
-                .then((response) => response.json())
-                .then(
-                  (response: {
-                    TrainAnnouncement: TrainAnnouncement[];
-                    INFO: INFO;
-                  }) => {
-                    setAnnouncements(response.TrainAnnouncement);
-                  }
-                )
-            }
-          >
-            fetch
-          </button>
-          <table>
-            {announcements.map((announcement: TrainAnnouncement) => (
-              <tr>
-                <td>{announcement.ActivityType}</td>
-                <td>{announcement.LocationSignature}</td>
-                <td>{trim(announcement.AdvertisedTimeAtLocation)}</td>
-                <td>{trim(announcement.TimeAtLocationWithSeconds)}</td>
-              </tr>
-            ))}
-          </table>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <button
+        type="button"
+        onClick={() =>
+          fetch(
+            "/.netlify/functions/node-fetch?trainId=7228&until=2021-09-11T23:59:59"
+          )
+            .then((response) => response.json())
+            .then(
+              (response: {
+                TrainAnnouncement: TrainAnnouncement[];
+                INFO: INFO;
+              }) => {
+                setAnnouncements(response.TrainAnnouncement);
+              }
+            )
+        }
+      >
+        fetch
+      </button>
+      <table>
+        {announcements.map((announcement: TrainAnnouncement) => (
+          <tr>
+            <td>{announcement.ActivityType}</td>
+            <td>{announcement.LocationSignature}</td>
+            <td>{trim(announcement.AdvertisedTimeAtLocation)}</td>
+            <td>{trim(announcement.TimeAtLocationWithSeconds)}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
